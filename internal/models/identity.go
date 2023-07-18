c79b2ef1e7c0d0a53f47013623ab6ade4950a246
Josh Rickard <jrickard@appomni.com>
2023-07-17 22:22:35 -0500

    fix: Added Identity model

--- internal/models/identity.go
+++ internal/models/identity.go
@@ -1,8 +1,31 @@
 package models
 
-type Identity struct {
+import (
+	"fmt"
+)
+
+type Identity interface {
+}
+
+type IdentityObject struct {
 	BaseModel
 	// These are properties from the MITRE ATT&CK json
 	ObjectMarkingRefs []string `json:"object_marking_refs"`
 	IdentityClass     string   `json:"identity_class"`
+}
+
+func NewIdentity(object map[string]interface{}) (IdentityObject, error) {
+	identity := IdentityObject{}
+	baseModel, err := parseBaseModel(object)
+	if err != nil {
+		slogger.Error(fmt.Sprintf("Error parsing base model: %s", err))
+	}
+	identity.BaseModel = baseModel
+	if object["object_marking_refs"] != nil {
+		identity.ObjectMarkingRefs = ConvertInterfaceArrayToStringArray(object["object_marking_refs"].([]interface{}))
+	}
+	if object["identity_class"] != nil {
+		identity.IdentityClass = object["identity_class"].(string)
+	}
+	return identity, nil
 }
