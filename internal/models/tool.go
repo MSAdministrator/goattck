727c600141891015f4edea8dbdc65d84747f1f66
Josh Rickard <jrickard@appomni.com>
2023-07-17 22:23:23 -0500

    fix: Added tool model

--- internal/models/tool.go
+++ internal/models/tool.go
@@ -1,21 +1,75 @@
 package models
 
-type Tool struct {
-	Description        string   `json:"description"`
+import (
+	"fmt"
+)
+
+type Tool interface {
+	Actors() ([]Actor, error)
+	Campaigns() ([]Campaign, error)
+	Techniques() ([]Technique, error)
+}
+
+type ToolObject struct {
+	BaseModel
+	BaseAttributes
 	XMitrePlatforms    []string `json:"x_mitre_platforms,omitempty"`
-	XMitreDeprecated   bool     `json:"x_mitre_deprecated,omitempty"`
 	XMitreContributors []string `json:"x_mitre_contributors,omitempty"`
 	XMitreAliases      []string `json:"x_mitre_aliases,omitempty"`
-	CreatedByRef       string   `json:"created_by_ref"`
-	Revoked            bool     `json:"revoked,omitempty"`
-	ExternalReferences []struct {
-		SourceName  string `json:"source_name"`
-		URL         string `json:"url"`
-		ExternalID  string `json:"external_id,omitempty"`
-		Description string `json:"description,omitempty"`
-	} `json:"external_references"`
-	ObjectMarkingRefs       []string `json:"object_marking_refs"`
+	ExternalReferences []ExternalReference `json:"external_references"`
 	Labels                  []string `json:"labels"`
 	XMitreAttackSpecVersion string   `json:"x_mitre_attack_spec_version"`
 	XMitreModifiedByRef     string   `json:"x_mitre_modified_by_ref"`
+}
+
+func NewTool(object map[string]interface{}) (ToolObject, error) {
+	tool := ToolObject{}
+	baseModel, err := parseBaseModel(object)
+	if err != nil {
+		slogger.Error(fmt.Sprintf("Error parsing base model: %s", err))
+	}
+	tool.BaseModel = baseModel
+	baseAttributes, err := parseBaseAttributes(object)
+	if err != nil {
+		slogger.Error(fmt.Sprintf("Error parsing base attributes: %s", err))
+	}
+	tool.BaseAttributes = baseAttributes
+	if object["x_mitre_platforms"] != nil {
+		tool.XMitrePlatforms = ConvertInterfaceArrayToStringArray(object["x_mitre_platforms"].([]interface{}))
+	}
+	if object["x_mitre_contributors"] != nil {
+		tool.XMitreContributors = ConvertInterfaceArrayToStringArray(object["x_mitre_contributors"].([]interface{}))
+	}
+	if object["x_mitre_aliases"] != nil {
+		tool.XMitreAliases = ConvertInterfaceArrayToStringArray(object["x_mitre_aliases"].([]interface{}))
+	}
+	if object["external_references"] != nil {
+		refs, err := parseExternalReferences(object)
+		if err != nil {
+			slogger.Error(fmt.Sprintf("Error parsing external references: %s", err))
+		}
+		tool.ExternalReferences = refs
+	}
+	if object["labels"] != nil {
+		tool.Labels = ConvertInterfaceArrayToStringArray(object["labels"].([]interface{}))
+	}
+	if object["x_mitre_attack_spec_version"] != nil {
+		tool.XMitreAttackSpecVersion = object["x_mitre_attack_spec_version"].(string)
+	}
+	if object["x_mitre_modified_by_ref"] != nil {
+		tool.XMitreModifiedByRef = object["x_mitre_modified_by_ref"].(string)
+	}
+	return tool, nil
+}
+
+func (t *ToolObject) Actors() ([]Actor, error) {
+	return nil, nil
+}
+
+func (t *ToolObject) Campaigns() ([]Campaign, error) {
+	return nil, nil
+}
+
+func (t *ToolObject) Techniques() ([]Technique, error) {
+	return nil, nil
 }
