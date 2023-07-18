3de2ea0497d9fe7442f116bd2f9797a9b7a05689
Josh Rickard <jrickard@appomni.com>
2023-07-17 22:18:59 -0500

    Added logo and update root

--- cmd/root.go
+++ cmd/root.go
@@ -1,23 +1,35 @@
 /*
-Copyright © 2023 NAME HERE <EMAIL ADDRESS>
+Copyright © 2023 Josh Rickard @MSAdministrator
 */
 package cmd
 
 import (
 	"os"
+	"fmt"
+	"encoding/json"
+
+	"github.com/spf13/cobra"
 
 	"github.com/msadministrator/goattck/internal/logger"
-	"github.com/spf13/cobra"
 )
 
 var slogger = logger.NewLogger(logger.Info, true)
+
+const logo = `                          
+                      __    __          __    
+   ____   _________ _/  |__/  |_  ____ |  | __
+  / ___\ /  _ \__  \\   __\   __\/ ___\|  |/ /
+ / /_/  >  <_> ) __ \|  |  |  | \  \___|    < 
+ \___  / \____(____  /__|  |__|  \___  >__|_ \
+/_____/            \/                \/     \/
+`
 
 // rootCmd represents the base command when called without any subcommands
 var rootCmd = &cobra.Command{
 	Use:   "goattck",
 	Short: "A golang CLI for the MITRE ATT&CK Framework",
-	Long: `This is a golang CLI for the MITRE ATT&CK Framework.
-	It utilizes work from pyattck and the MITRE ATT&CK API.`,
+	Long: ReturnLogo() + `This is a golang CLI for the MITRE ATT&CK Framework.
+It utilizes work from pyattck and the MITRE ATT&CK API`,
 }
 
 // Execute adds all child commands to the root command and sets flags appropriately.
@@ -30,41 +42,15 @@ func Execute() {
 }
 
 func init() {
-	// Here you will define your flags and configuration settings.
-	// Cobra supports persistent flags, which, if defined here,
-	// will be global for your application.
-
-	// rootCmd.PersistentFlags().StringVar(&cfgFile, "config", "", "config file (default is $HOME/.goattck.yaml)")
-
-	// Cobra also supports local flags, which will only run
-	// when this action is called directly.
-	rootCmd.Flags().BoolP("toggle", "t", false, "Help message for toggle")
+	// Persistent Flags
+	rootCmd.PersistentFlags().BoolP("pretty", "p", false, "Pretty print the output")
 }
 
-// type MitreFramework interface {
-// 	Actors() ([]Actor, error)
-// 	Campaigns() ([]Campaign, error)
-// 	Controls() ([]Control, error)
-// 	DataComponents() ([]DataComponent, error)
-// 	DataSources() ([]DataSource, error)
-// 	Malwares() ([]Malware, error)
-// 	Mitigations() ([]Mitigation, error)
-// 	Relationships() ([]Relationship, error)
-// 	Tactics() ([]Tactic, error)
-// 	Techniques() ([]Technique, error)
-// 	Tools() ([]Tool, error)
-// }
+func PrettyPrint(i interface{}) string {
+	s, _ := json.MarshalIndent(i, "", "\t")
+	return string(s)
+}
 
-// type MitreFramework interface {
-// 	Actors() ([]Actor, error)
-// 	Campaigns() ([]Campaign, error)
-// 	Controls() ([]Control, error)
-// 	DataComponents() ([]DataComponent, error)
-// 	DataSources() ([]DataSource, error)
-// 	Malwares() ([]Malware, error)
-// 	Mitigations() ([]Mitigation, error)
-// 	Relationships() ([]Relationship, error)
-// 	Tactics() ([]Tactic, error)
-// 	Techniques() ([]Technique, error)
-// 	Tools() ([]Tool, error)
-// }
+func ReturnLogo() string {
+	return fmt.Sprintf("\u200B %s", logo)
+}
