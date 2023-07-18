b11b09269ae2147cc9eec86e9aae9a795b672ddb
Josh Rickard <jrickard@appomni.com>
2023-07-17 22:21:09 -0500

    fix: Adding actor model

--- internal/models/actor.go
+++ internal/models/actor.go
@@ -5,25 +5,24 @@ import (
 	"reflect"
 )
 
-type Actor []struct {
+type Actor interface {
+	Malwares() ([]Malware, error)
+	Tools() ([]Tool, error)
+	Techniques() ([]Technique, error)
+}
+
+type ActorObject struct {
 	BaseModel
+	BaseAttributes
+	BaseExternalModel
 	// These are properties from the MITRE ATT&CK json
-	Description        string   `json:"description,omitempty"`
-	Aliases            []string `json:"aliases,omitempty"`
-	XMitreDeprecated   bool     `json:"x_mitre_deprecated,omitempty"`
 	XMitreContributors []string `json:"x_mitre_contributors,omitempty"`
-	CreatedByRef       string   `json:"created_by_ref,omitempty"`
-	Revoked            bool     `json:"revoked,omitempty"`
-	ExternalReferences []struct {
-		SourceName  string `json:"source_name"`
-		URL         string `json:"url,omitempty"`
-		ExternalID  string `json:"external_id,omitempty"`
-		Description string `json:"description,omitempty"`
-	} `json:"external_references"`
-	ObjectMarkingRefs       []string `json:"object_marking_refs,omitempty"`
-	XMitreAttackSpecVersion string   `json:"x_mitre_attack_spec_version,omitempty"`
-	XMitreModifiedByRef     string   `json:"x_mitre_modified_by_ref,omitempty"`
 	// These are properties unique to pyattck-data
+	actorExternalAttributes
+	MitreAttckId string `json:"mitre_attck_id"`
+}
+
+type actorExternalAttributes struct {
 	Names               []string `json:"names"`
 	ExternalTools       []string `json:"external_tools"`
 	Country             []string `json:"country"`
@@ -31,48 +30,92 @@ type Actor []struct {
 	Links               []string `json:"links"`
 	Targets             []string `json:"targets"`
 	ExternalDescription []string `json:"external_description"`
-	AttckID             string   `json:"attck_id"`
+	AttckID				string   `json:"attck_id"`
 	Comment             string   `json:"comment"`
+	Comments			[]string `json:"comments"`
 }
 
-func NewActor(object struct{}) (Actor, error) {
-	actor := Actor{
-		EnterpriseAttck: EnterpriseAttck{
-			// These are properties from the MITRE ATT&CK json
-			Type:        object.Type,
-			ID:          object.ID,
-			Name:        object.Name,
-			Description: object.Description,
-		},
+func NewActor(object map[string]interface{}) (ActorObject, error) {
+	baseModel, err := parseBaseModel(object)
+	if err != nil {
+		slogger.Error(fmt.Sprintf("Error parsing base model: %s", err))
 	}
-	slogger.Info(fmt.Sprintf("Inside NewActor. Created actor: %+v", actor.AttckID))
+	baseAttributes, err := parseBaseAttributes(object)
+	if err != nil {
+		slogger.Error(fmt.Sprintf("Error parsing base attributes: %s", err))
+	}
+	baseExternalModel, err := parseExternalModel(object)
+	if err != nil {
+		slogger.Error(fmt.Sprintf("Error parsing external model: %s", err))
+	}
+	aExternalAttributes, err := parseActorExternalAttributes(object)
+	if err != nil {
+		slogger.Error(fmt.Sprintf("Error parsing external attributes: %s", err))
+	}
+	actor := ActorObject{
+		actorExternalAttributes: aExternalAttributes,
+	}
+	actor.BaseModel = baseModel
+	actor.BaseAttributes = baseAttributes
+	actor.BaseExternalModel = baseExternalModel
+	if object["x_mitre_contributors"] != nil {
+		actor.XMitreContributors = ConvertInterfaceArrayToStringArray(object["x_mitre_contributors"].([]interface{}))
+	}
+	for _, extRef := range actor.ExternalReferences {
+		if extRef.SourceName == "mitre-attack" {
+			actor.MitreAttckId = extRef.ExternalId
+		}
+	}
 	return actor, nil
 }
 
-func (a *Actor) Malwares() ([]Malware, error) {
+func (a *ActorObject) Malwares() ([]Malware, error) {
 	return nil, nil
 }
 
-func (a *Actor) Tools() ([]Tool, error) {
+func (a *ActorObject) Tools() ([]Tool, error) {
 	return nil, nil
 }
 
-func (a *Actor) Techniques() ([]Technique, error) {
+func (a *ActorObject) Techniques() ([]Technique, error) {
 	return nil, nil
 }
 
-func ObjectAssign(target interface{}, object interface{}) {
-	// object atributes values in target atributes values
-	// using pattern matching (https://golang.org/pkg/reflect/#Value.FieldByName)
-	// https://stackoverflow.com/questions/35590190/how-to-use-the-spread-operator-in-golang
-	t := reflect.ValueOf(target).Elem()
-	o := reflect.ValueOf(object).Elem()
-	for i := 0; i < o.NumField(); i++ {
-		for j := 0; j < t.NumField(); j++ {
-			if t.Field(j) == o.Field(i) {
-				fmt.Printf("Field %s is equal to %s\n", t.Field(j), o.Field(i))
-				t.Field(j).Set(o.Field(i))
-			}
-		}
+func (a *ActorObject) ToConsole() {
+	headers := []string{"Id", "Name", "Type", "Description"}
+	rows := []string{a.Id, a.Name, a.Type, a.Description}
+	DisplayOutput(headers, rows)
+}
+
+// Parses the external attributes of the actor
+func parseActorExternalAttributes(object map[string]interface{}) (actorExternalAttributes, error) {
+	aExternalAttributes := actorExternalAttributes{}
+	if object["external_tools"] != nil {
+		aExternalAttributes.ExternalTools = ConvertInterfaceArrayToStringArray(object["external_tools"].([]interface{}))
 	}
+	if object["country"] != nil {
+		aExternalAttributes.Country = ConvertInterfaceArrayToStringArray(object["country"].([]interface{}))
+	}
+	if object["operations"] != nil {
+		aExternalAttributes.Operations = ConvertInterfaceArrayToStringArray(object["operations"].([]interface{}))
+	}
+	if object["links"] != nil {
+		aExternalAttributes.Links = ConvertInterfaceArrayToStringArray(object["links"].([]interface{}))
+	}
+	if object["targets"] != nil {
+		aExternalAttributes.Targets = ConvertInterfaceArrayToStringArray(object["targets"].([]interface{}))
+	}
+	if object["external_description"] != nil {
+		aExternalAttributes.ExternalDescription = ConvertInterfaceArrayToStringArray(object["external_description"].([]interface{}))
+	}
+	if object["attck_id"] != nil {
+		aExternalAttributes.AttckID = object["attck_id"].(string)
+	}
+	if object["comment"] != nil {
+		aExternalAttributes.Comment = object["comment"].(string)
+	}
+	if object["comments"] != nil {
+		aExternalAttributes.Comments = ConvertInterfaceArrayToStringArray(object["comments"].([]interface{}))
+	}
+	return aExternalAttributes, nil
 }
