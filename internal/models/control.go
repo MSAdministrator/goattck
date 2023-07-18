af721f46573a906b5b07ed829aa3d53c67831d2c
Josh Rickard <jrickard@appomni.com>
2023-07-17 22:21:50 -0500

    fix: Added control model

--- internal/models/control.go
+++ internal/models/control.go
@@ -1,6 +1,12 @@
 package models
 
-type Control struct {
+import "fmt"
+
+type Control interface {
+	Techniques() ([]Technique, error)
+}
+
+type ControlObject struct {
 	// Base fields
 	BaseModel
 	// Fields
@@ -9,9 +15,41 @@ type Control struct {
 	XMitreImpact       []string             `json:"x_mitre_impact"`
 	XMitrePriority     string               `json:"x_mitre_priority"`
 	ObjectMarkingRefs  []string             `json:"object_marking_refs"`
-	ExternalReferences []ExternalReferences `json:"external_references"`
+	ExternalReferences []ExternalReference `json:"external_references"`
 }
 
-func (c *Control) Techniques() ([]Technique, error) {
+func NewControl(object map[string]interface{}) (ControlObject, error) {
+	control := ControlObject{}
+	baseModel, err := parseBaseModel(object)
+	if err != nil {
+		slogger.Error(fmt.Sprintf("Error parsing base model: %s", err))
+	}
+	control.BaseModel = baseModel
+	if object["revoked"] != nil {
+		control.Revoked = object["revoked"].(bool)
+	}
+	if object["x_mitre_family"] != nil {
+		control.XMitreFamily = object["x_mitre_family"].(string)
+	}
+	if object["x_mitre_impact"] != nil {
+		control.XMitreImpact = object["x_mitre_impact"].([]string)
+	}
+	if object["x_mitre_priority"] != nil {
+		control.XMitrePriority = object["x_mitre_priority"].(string)
+	}
+	if object["object_marking_refs"] != nil {
+		control.ObjectMarkingRefs = object["object_marking_refs"].([]string)
+	}
+	if object["external_references"] != nil {
+		refs, err := parseExternalReferences(object)
+		if err != nil {
+			slogger.Error(fmt.Sprintf("Error parsing external references: %s", err))
+		}
+		control.ExternalReferences = refs
+	}
+	return control, nil
+}
+
+func (c *ControlObject) Techniques() ([]Technique, error) {
 	return nil, nil
 }
